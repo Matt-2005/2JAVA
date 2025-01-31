c2f4c65f6de309294069cf82549bf8f222802b29
Matthew LE <matthew.le548@gmail.com>
2025-01-31 22:54:23 +0100

    gestion magasin/User ok

--- com/iStore/AdminDAO.java
+++ com/iStore/AdminDAO.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.xml.crypto.Data;
+
 public class AdminDAO {
     public boolean createStore(int ID, String Name) throws SQLException{
         String requeteSQL = "INSERT INTO STORE (ID, NAME) VALUES (?, ?)";
@@ -32,5 +34,29 @@ public class AdminDAO {
                 }
             }
         return false;
+    }
+
+    public boolean deleteStore(String Name) throws SQLException {
+        String requeteSQL = "DELETE FROM STORE WHERE NAME = ?";
+
+        try (Connection conn = DatabaseConfig.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(requeteSQL)) {
+                pstmt.setString(1, Name);
+                int affectedRows = pstmt.executeUpdate();
+
+                return affectedRows > 0;
+            }
+    }
+
+    public boolean deleteUser(String Pseudo) throws SQLException {
+        String requeteSQL = "DELETE FROM USER WHERE PSEUDO = ?";
+
+        try (Connection conn = DatabaseConfig.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(requeteSQL)) {
+                pstmt.setString(1, Pseudo);
+                int affectedRows = pstmt.executeUpdate();
+
+                return affectedRows > 0;
+            }
     }
 }
--- com/iStore/GraphicInterface.java
+++ com/iStore/GraphicInterface.java
@@ -2,15 +2,10 @@ package com.iStore;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.xml.crypto.Data;
 
 import java.awt.*;
-import java.beans.Statement;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.sql.*;
+import java.util.*;
 import java.util.List;
 
 public class GraphicInterface {
@@ -35,7 +30,11 @@ public class GraphicInterface {
         mainPanel.add(createAdminPanel(), "AdminDashboard");
         mainPanel.add(createEmployeePanel(), "EmployeePanel");
         mainPanel.add(createStorePanel(), "StoreCreation");
-        mainPanel.add(createDisplayStores(), "DisplayStore");
+        mainPanel.add(createDisplayStoresPanel(), "DisplayStore");
+        mainPanel.add(createDeleteStorePanel(), "DeleteStore");
+        mainPanel.add(createUserPanel(), "AddUser");
+        mainPanel.add(createDeleteUserPanel(), "DeleteUser");
+        mainPanel.add(createDisplayUserPanel(), "DisplayUser");
 
         // Ajouter le panneau principal à la fenêtre
         frame.add(mainPanel);
@@ -175,6 +174,7 @@ public class GraphicInterface {
         JButton createStoreBtn = new JButton("Créer un magasin");
         createStoreBtn.addActionListener(e -> cardLayout.show(mainPanel, "StoreCreation"));
         JButton deleteStoreBtn = new JButton("Supprimer un magasin");
+        deleteStoreBtn.addActionListener(e -> cardLayout.show(mainPanel, "DeleteStore"));
         JButton listStoreBtn = new JButton("Voir les magasins");
         listStoreBtn.addActionListener(e -> cardLayout.show(mainPanel, "DisplayStore"));
         storePanel.add(createStoreBtn);
@@ -185,8 +185,11 @@ public class GraphicInterface {
         JPanel employeePanel = new JPanel();
         employeePanel.setBorder(BorderFactory.createTitledBorder("Gestion des Employés"));
         JButton addEmployeeBtn = new JButton("Ajouter un employé");
+        addEmployeeBtn.addActionListener(e -> cardLayout.show(mainPanel, "AddUser"));
         JButton removeEmployeeBtn = new JButton("Supprimer un employé");
+        removeEmployeeBtn.addActionListener(e -> cardLayout.show(mainPanel, "DeleteUser"));
         JButton listEmployeeBtn = new JButton("Voir les employés");
+        listEmployeeBtn.addActionListener(e -> cardLayout.show(mainPanel, "DisplayUser"));
         employeePanel.add(addEmployeeBtn);
         employeePanel.add(removeEmployeeBtn);
         employeePanel.add(listEmployeeBtn);
@@ -311,7 +314,7 @@ public class GraphicInterface {
         return panel;
     }
 
-    private JPanel createDisplayStores() {
+    private JPanel createDisplayStoresPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
         // Titre
@@ -353,7 +356,7 @@ public class GraphicInterface {
         // Panel pour les boutons
         JPanel buttonPanel = new JPanel();
         JButton backButton = new JButton("Retour");
-        
+
         // Bouton Retour vers AdminDashboard
         backButton.addActionListener(e -> cardLayout.show(mainPanel, "AdminDashboard"));
 
@@ -365,6 +368,268 @@ public class GraphicInterface {
 
         return panel;
     } 
+
+    private JPanel createDeleteStorePanel() {
+        AdminDAO adminDAO = new AdminDAO();
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 3, 10, 10));
+        // Titre
+        JLabel titleLabel = new JLabel("Liste des magasins", JLabel.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        panel.add(titleLabel, BorderLayout.NORTH);
+
+        // Définition des colonnes
+        String[] columnNames = {"ID", "Nom"};
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0); // Modèle de tableau vide
+
+        List<Store> stores = new ArrayList<>();
+        
+        String requeteSQL = "SELECT ID, NAME FROM STORE";
+        try (Connection conn = DatabaseConfig.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(requeteSQL);
+            ResultSet rs = pstmt.executeQuery()) {
+
+                while (rs.next()) {
+                    int Id = rs.getInt("ID");
+                    String Name = rs.getString("NAME");
+
+                    stores.add(new Store(Id, Name));
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(panel, "Erreur SQL : " + e.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+            }
+        
+        // **Remplir le tableau avec les données de la liste**
+        for (Store store : stores) {
+            model.addRow(new Object[]{store.getId(), store.getName()});
+        }
+
+        // Création du JTable avec le modèle dynamique
+        JTable employeeTable = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(employeeTable);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        panel.add(new JLabel("Nom du magasin a suprimer :", JLabel.CENTER));
+        JTextField nameField = new JTextField();
+        panel.add(nameField);
+        // Panel pour les boutons
+        JPanel buttonPanel = new JPanel();
+        JButton backButton = new JButton("Retour");
+        JButton validationButton = new JButton("Suprimer");
+        
+        // Bouton Retour vers AdminDashboard
+        backButton.addActionListener(e -> cardLayout.show(mainPanel, "AdminDashboard"));
+        validationButton.addActionListener(e -> {
+            try {
+                String Name = nameField.getText();
+                if (adminDAO.deleteStore(Name)) {
+                    JOptionPane.showMessageDialog(panel, "Magasin " + Name + " supprimé avec succès !.", "iStore", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(panel, "Erreur lors de la suppression du magasin.", "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (Exception ex) {
+                System.out.println("Erreur lors de la suppression du magasin : " + ex.getMessage());
+            }
+            
+        });
+
+        // Ajouter les boutons au panel des boutons
+        buttonPanel.add(backButton);
+        buttonPanel.add(validationButton);
+
+        panel.add(buttonPanel, BorderLayout.CENTER);
+        
+
+        return panel;
+    } 
+
+    private JPanel createUserPanel() {
+        UserDAO userDAO = new UserDAO();
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 2, 10, 10));
+
+        panel.add(new JLabel("Email :"));
+        JTextField emailField = new JTextField();
+        panel.add(emailField);
+
+        panel.add(new JLabel("Pseudo :"));
+        JTextField pseudoField = new JTextField();
+        panel.add(pseudoField);
+
+        panel.add(new JLabel("Mot de passe :"));
+        JPasswordField passwordField = new JPasswordField();
+        panel.add(passwordField);
+
+        JButton backButton = new JButton("Retour");
+        backButton.addActionListener(e -> cardLayout.show(mainPanel, "AdminDashboard"));
+
+        JButton signUpButton = new JButton("S'inscrire");
+
+        signUpButton.addActionListener(e -> {
+            String Email = emailField.getText();
+            String Pseudo = pseudoField.getText();
+            String Password = new String(passwordField.getPassword());
+            try{
+                PasswordHash.HashResults hashResults = PasswordHash.passwordHash(Password);
+    
+                User user = new User(0, Email, Pseudo, hashResults.getHashedPassword(), hashResults.getSalt(), null);
+    
+                if (userDAO.verifyEmail(Email)) {
+                    JOptionPane.showMessageDialog(panel, "Email déja utilisé, veuillez vous connecter.", "Erreur", JOptionPane.ERROR_MESSAGE);
+
+                }
+                if (userDAO.createUser(user)) {
+                    if (user.getRole() == "Employé") {
+                        cardLayout.show(mainPanel, "EmployeePanel");
+                    } else {
+                        cardLayout.show(mainPanel, "AdminDashboard");
+                    }
+                    
+                }
+                
+            } catch (Exception ex) {
+                System.out.println("Erreur lors de la création du compte : " + ex.getMessage());
+            }
+        });
+
+        panel.add(backButton);
+        panel.add(signUpButton);
+
+        return panel;
+    }
+
+    private JPanel createDeleteUserPanel() {
+        AdminDAO adminDAO = new AdminDAO();
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 3, 10, 10));
+        // Titre
+        JLabel titleLabel = new JLabel("Liste des utilisateurs", JLabel.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        panel.add(titleLabel, BorderLayout.NORTH);
+
+        // Définition des colonnes
+        String[] columnNames = {"ID", "Email", "Pseudo", "Rôle"};
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0); // Modèle de tableau vide
+
+        List<User> employees = new ArrayList<>();
+        
+        String requeteSQL = "SELECT ID, EMAIL, PSEUDO, ROLE FROM USER";
+        try (Connection conn = DatabaseConfig.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(requeteSQL);
+            ResultSet rs = pstmt.executeQuery()) {
+
+                while (rs.next()) {
+                    int Id = rs.getInt("ID");
+                    String Email = rs.getString("EMAIL");
+                    String Pseudo = rs.getString("PSEUDO");
+                    String Role = rs.getString("ROLE");
+
+                    employees.add(new User(Id, Email, Pseudo, "", "", Role));
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(panel, "Erreur SQL : " + e.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+            }
+        
+        // **Remplir le tableau avec les données de la liste**
+        for (User user : employees) {
+            model.addRow(new Object[]{user.getId(), user.getEmail(), user.getPseudo(), user.getRole()});
+        }
+
+        // Création du JTable avec le modèle dynamique
+        JTable employeeTable = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(employeeTable);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        panel.add(new JLabel("Pseudo de l'utilisateur à suprimer :", JLabel.CENTER));
+        JTextField nameField = new JTextField();
+        panel.add(nameField);
+        // Panel pour les boutons
+        JPanel buttonPanel = new JPanel();
+        JButton backButton = new JButton("Retour");
+        JButton validationButton = new JButton("Suprimer");
+        
+        // Bouton Retour vers AdminDashboard
+        backButton.addActionListener(e -> cardLayout.show(mainPanel, "AdminDashboard"));
+        validationButton.addActionListener(e -> {
+            try {
+                String Pseudo = nameField.getText();
+                if (adminDAO.deleteUser(Pseudo)) {
+                    JOptionPane.showMessageDialog(panel, "Utilisateur " + Pseudo + " supprimé avec succès !.", "iStore", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(panel, "Erreur lors de la suppression de l'utilisateur.", "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (Exception ex) {
+                System.out.println("Erreur lors de la suppression de l'utilisateur : " + ex.getMessage());
+            }
+            
+        });
+
+        // Ajouter les boutons au panel des boutons
+        buttonPanel.add(backButton);
+        buttonPanel.add(validationButton);
+
+        panel.add(buttonPanel, BorderLayout.CENTER);
+        
+
+        return panel;
+    }
+
+    private JPanel createDisplayUserPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        // Titre
+        JLabel titleLabel = new JLabel("Panneau Utilisateurs", JLabel.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        panel.add(titleLabel, BorderLayout.NORTH);
+
+        // Définition des colonnes
+        String[] columnNames = {"ID", "Email", "Pseudo", "Rôle"};
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0); // Modèle de tableau vide
+
+        List<User> employees = new ArrayList<>();
+        
+        String requeteSQL = "SELECT ID, EMAIL, PSEUDO, ROLE FROM USER";
+        try (Connection conn = DatabaseConfig.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(requeteSQL);
+            ResultSet rs = pstmt.executeQuery()) {
+
+                while (rs.next()) {
+                    int Id = rs.getInt("ID");
+                    String Email = rs.getString("EMAIL");
+                    String Pseudo = rs.getString("PSEUDO");
+                    String Role = rs.getString("ROLE");
+
+                    employees.add(new User(Id, Email, Pseudo, "", "", Role));
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(panel, "Erreur SQL : " + e.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+            }
+        
+        // **Remplir le tableau avec les données de la liste**
+        for (User user : employees) {
+            model.addRow(new Object[]{user.getId(), user.getEmail(), user.getPseudo(), user.getRole()});
+        }
+
+        // Création du JTable avec le modèle dynamique
+        JTable employeeTable = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(employeeTable);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        // Panel pour les boutons
+        JPanel buttonPanel = new JPanel();
+        JButton backButton = new JButton("Retour");
+
+        // Bouton Retour vers AdminDashboard
+        backButton.addActionListener(e -> cardLayout.show(mainPanel, "AdminDashboard"));
+
+        // Ajouter les boutons au panel des boutons
+        buttonPanel.add(backButton);
+
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+        
+
+        return panel;
+    }
 
     
 
